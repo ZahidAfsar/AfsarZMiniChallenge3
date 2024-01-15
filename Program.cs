22798cf168f3296aa76ce4d8b6ac34b97384329c
Zahid Afsar <zahidtempz@icloud.com>
2024-01-14 17:43:51 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Zahid Afsar
+// 9/8/23
+// Mini Challenge 3
+// Ask 2 questions and then put the answer in one sentence
+// Peer reviewed by : Micahel Pantoja              Peer Comment : it run really well has no errors 
+
+Console.Clear();
+
+Console.WriteLine("What is your Name?");
+
+string userName = Console.ReadLine();
+
+Console.WriteLine("What is the time you woke up?");
+
+string askTime = Console.ReadLine();
+
+Console.WriteLine("Hello " + userName + " you woke up at " + askTime);
+
